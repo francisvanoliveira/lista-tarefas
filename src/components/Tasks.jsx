1de35e529028fbc95e538a7fea474c0e309443b1
Francisvan Oliveira <francisvan.oliveira@francisvanoliveira.com.br>
2024-10-10 17:17:57 +0000

    Navegação

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
 
 function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
+    const navigates = useNavigate();
+
+    function onSeeDetailsClick(task){
+        const query = new URLSearchParams()
+        query.set("title", task.title);
+        query.set("description", task.description);
+
+        navigates(`/task?id=${query.toString()}}`);
+    }
+
     return(
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
@@ -8,7 +18,7 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
                     <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
                         {task.title}
                     </button>
-                    <button className="bg-slate-400 text-white p-2 rounded-md">
+                    <button onClick={() => onSeeDetailsClick(task)} className="bg-slate-400 text-white p-2 rounded-md">
                         <ChevronRightIcon />
                     </button>
                     <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 text-white p-2 rounded-md">

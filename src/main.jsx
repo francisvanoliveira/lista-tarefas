066b60b4a3eaadf7ff65a0fca2f3f5a7872c005e
Francisvan Oliveira <francisvan.oliveira@francisvanoliveira.com.br>
2024-10-10 14:27:06 -0400

    Atualização do codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TaskPage from "./pages/TaskPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,10 +14,10 @@ const router = createBrowserRouter([
     path: "/task",
     element: <TaskPage />,
   },
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
